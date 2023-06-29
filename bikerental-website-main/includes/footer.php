35a5bd9959590a2a49999b1ebee4552f54788a95
sayfn <sayfoullahnejjar@gmail.com>
2023-06-29 14:23:22 +0200

    Footer change

--- bikerental-website-main/includes/footer.php
+++ bikerental-website-main/includes/footer.php
@@ -62,7 +62,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
           </div>
         </div>
         <div class="col-md-6 col-md-pull-6">
-          <p class="copy-right">Copyright &copy; 2022 Online Bike Rental</a>.</p>
+          <p class="copy-right">Copyright &copy; 2023 Online Bike Sharing</a>.</p>
         </div>
       </div>
     </div>
